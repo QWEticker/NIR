8fb0fe817d5107f4b5ed7f9c4199426e1e73adbc
Alex Kon <konovalenko.ad@gmail.com>
2026-05-28 03:38:59 +0700

    chore: untrack build/ directory and update .gitignore

--- build/cvqkd_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/konov/Documents/NIR/build/cvqkd_tests[1]_tests.cmake")
-  include("C:/Users/konov/Documents/NIR/build/cvqkd_tests[1]_tests.cmake")
-else()
-  add_test(cvqkd_tests_NOT_BUILT cvqkd_tests_NOT_BUILT)
-endif()
